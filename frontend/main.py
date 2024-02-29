7ede5d77bc408b7ac9b98d8dc01aef0895af4e77
Clementine2722 <84880070+Clementine2722@users.noreply.github.com>
2024-02-29 09:51:00 -0600

    templates folder and AST to HTML

--- frontend/main.py
+++ frontend/main.py
@@ -4,7 +4,7 @@ from bleach import clean
 from os import listdir, remove
 from os import path as ospath
 
-def metadata_extractor(markdown_file):
+def metadata_extractor_AST(markdown_file):
     lock = 0
     for i in range(len(markdown_file)):
         if len(markdown_file[i]) > 1:
@@ -14,6 +14,22 @@ def metadata_extractor(markdown_file):
                 lock = 1
                 break
     if lock:
+        return (data, metadata)
+        
+    else:
+        return 0
+
+def metadata_extractor_HTML(markdown_file):
+    lock = 0
+    for i in range(len(markdown_file,HTML_file)):
+        if len(markdown_file[i]) > 1:
+            if list(markdown_file[i])[1] == "style":
+                metadata = [item['raw'] for item in markdown_file[i]['children'] if 'raw' in item]
+                lock = 1
+                break
+    if lock:
+        data = HTML_file.split("\n")
+        data.pop(1)
         return (data, metadata)
         
     else:
@@ -32,12 +48,17 @@ scenarios_names = [a.split(".")[0] for a in scenarios_paths]
 scenarios=[]
 output_scenarios_paths = []
 output_scenarios_names = []
+scenarios_data=[]
 for file_path, pathandname in zip(files_paths,zip(scenarios_paths,scenarios_names)):
     with open(file_path, 'r') as file:
-        scenario_AST = markdown(clean(file.read()),renderer="ast")
-    if metadata_extractor(scenario_AST):
-        SCE_data, SCE_metadata = metadata_extractor(scenario_AST)
+        File = file.read()
+    scenario_AST = markdown(clean(File),renderer="ast")
+    scenario_HTML = markdown(clean(File),renderer="html")
+    if metadata_extractor_AST(scenario_AST):
+        SCE_data, SCE_metadata = metadata_extractor_AST(scenario_AST)
         scenarios.append([SCE_metadata,SCE_data])
+        SCE_data, SCE_metadata = metadata_extractor_HTML(scenario_AST,scenario_HTML)
+        scenarios_data.append([SCE_metadata,SCE_data])
         output_scenarios_paths.append(pathandname[0])
         output_scenarios_names.append(pathandname[1])
 ###
@@ -45,23 +66,33 @@ for file_path, pathandname in zip(files_paths,zip(scenarios_paths,scenarios_name
 ###     OPENING ALL PROBLEMS
 files_paths = [path+"/problems/"+a for a in listdir(path+"/problems")]
 problems=[]
+problems_data[]
 for file_path in files_paths:
     with open(file_path, 'r') as file:
-        problem_AST = markdown(clean(file.read()),renderer="ast")
-    if metadata_extractor(problem_AST):
-        PRO_data, PRO_metadata = metadata_extractor(problem_AST)
+        File = file.read()
+    problem_AST = markdown(clean(File),renderer="ast")
+    problem_HTML = markdown(clean(File),renderer="html")
+    if metadata_extractor_AST(problem_AST):
+        PRO_data, PRO_metadata = metadata_extractor_AST(problem_AST)
         problems.append([PRO_metadata,PRO_data])
+        PRO_data, PRO_metadata = metadata_extractor_HTML(problem_AST,problem_HTML)
+        problems_data.append([PRO_metadata,PRO_data])
 ###
 
 ###     OPENING ALL IDEAS
 files_paths = [path+"/ideas/"+a for a in listdir(path+"/ideas")]
 ideas = []
+ideas_data = []
 for file_path in files_paths:
     with open(file_path, 'r') as file:
-        idea_AST = markdown(clean(file.read()),renderer="ast")
-    if metadata_extractor(idea_AST):
-        IDE_data, IDE_metadata = metadata_extractor(idea_AST)
+        File = file.read()
+    idea_AST = markdown(clean(File),renderer="ast")
+    idea_HTML = markdown(clean(File),renderer="html")
+    if metadata_extractor_AST(idea_AST):
+        IDE_data, IDE_metadata = metadata_extractor_AST(idea_AST)
         ideas.append([IDE_metadata,IDE_data])
+        IDE_data, IDE_metadata = metadata_extractor_HTML(idea_AST,idea_HTML)
+        ideas_data.append([IDE_metadata,IDE_data])
 ###
 
 
@@ -102,12 +133,14 @@ for i in range(len(problems)):
 
     dictionary = {splited1[0]:int(splited1[1]),splited2[0]:splited2[1]}
     problems[i][0] = dictionary
+    problems_data[i][0] = dictionary
 
 for i in range(len(ideas)):
     element = ideas[i][0]
     splited = element[0].split(":")
     dictionary = {splited[0]:[int(splited[1])]}
     ideas[i][0] = dictionary
+    ideas_data[i][0] = dictionary
 
 for i in range(len(scenarios)):
     element = scenarios[i][0]
@@ -115,6 +148,7 @@ for i in range(len(scenarios)):
     splited[1] = list(map(int,splited[1].split(",")))
     dictionary = {splited[0]:splited[1]}
     scenarios[i][0] = dictionary
+    scenarios_data[i][0] = dictionary
 
 #Now they look like this
 
@@ -153,21 +187,21 @@ for i in range(len(scenarios)):
     placeholder = scenarios[i][0]['problems']
 
     Automator = []
-    for problem in problems:
+    for problem, problem_data in zip(problems,problems_data):
         if problem[0]["id"] in placeholder:
             ideas_md = []
-            for idea in ideas:
+            for idea in ideas_data:
                 if idea[0]["id"][0] in problem[0]["ideas"]:
                     ideas_md.append(idea[1])
 
-            Automator.append({"id":problem[0]["id"],"description":problem[1],"title":problem[0]["title"],"ideas":ideas_md})
+            Automator.append({"id":problem[0]["id"],"description":problem_data[1],"title":problem[0]["title"],"ideas":ideas_md})
 
-    with open('frontend\scenario_template.html', 'r') as file:
+    with open('frontend\\templates\scenario_template.html', 'r') as file:
         template_content = file.read()
 
-    environment = Environment(loader=FileSystemLoader("frontend"))
+    environment = Environment(loader=FileSystemLoader("frontend\\templates"))
     template = environment.get_template("scenario_template.html")
-    output = template.render(scenario_content=SCE_data,problems=Automator,scenario_name=output_scenarios_names[i])
+    output = template.render(scenario_content=scenarios_data[i][1],problems=Automator,scenario_name=output_scenarios_names[i])
 
 
     # problems must be a list of dicts with id:name, description:description
